071b77ee660a9e57b770c377dc83280991417f75
Aparecida Silva <cidinhalilisilva01@gmail.com>
2016-08-20 16:46:24 -0300

    Questão10.py

---
+++ Questão10.py
@@ -0,0 +1,10 @@
+'''
+Faça um programa que receba dois números inteiros e gere os números
+inteiros que estão no intervalo compreendido por eles.
+'''
+
+n1 = int(input("Informe o primeiro número: "))
+n2 = int(input("Informe o segundo número: "))
+
+for entreN in range (n1 , n2 , 1):
+    print (entreN)
